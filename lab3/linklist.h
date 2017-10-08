a4030b069abc5ae6ddf59c1292897962112a3ab3
a1135122775 <1135122775@qq.com>
2017-10-08 02:23:13 -0700

    menu 2.0

---
+++ lab3/linklist.h
@@ -0,0 +1,12 @@
+#ifndef _LINKLIST_H_
+#define _LINKLIST_H_
+typedef struct DataNode
+{
+	char *cmd;
+	char *desc;
+	int (*hander)();
+	struct DataNode *next;
+}tDataNode;
+tDataNode *FindCmd(tDataNode*head,char *cmd);
+int ShowAllCmd(tDataNode *head);
+#endif
